5683c061f6c72933bd1915bb7402f81bde6d2d0d
rhutmann <pappy.rhutmannjnrolege@gmail.com>
2022-12-11 10:31:58 +0000

    added services

--- Utils/writeRead.ts
+++ Utils/writeRead.ts
@@ -8,8 +8,8 @@ const file =
     : path.join('/temp', 'hook.json');
 
 export async function getWebHook() {
-  const files = await readdir(path.join('/temp', 'hook.json'));
-  for (const file1 of files) console.log(file1, 'filess');
+  // const files = await readdir(path.join('/temp', 'hook.json'));
+  // for (const file1 of files) console.log(file1, 'filess');
 
   // const rawFileContent = await fs.readFile(file, { encoding: 'utf-8' });
   // const data = JSON.parse(rawFileContent);
