56f3fde00d205972bbf4f3742e99d75c6ace9b63
rhutmann <pappy.rhutmannjnrolege@gmail.com>
2022-12-11 10:43:42 +0000

    added services

--- Utils/twitter.ts
+++ Utils/twitter.ts
@@ -8,7 +8,5 @@ export async function getMemeTweets() {
     'tweet.fields': ['public_metrics', 'created_at']
   });
 
-  console.log('response', JSON.stringify(response));
-
   return response;
 }
--- Utils/writeRead.ts
+++ Utils/writeRead.ts
@@ -8,11 +8,13 @@ const file =
     : path.join('/tmp', 'hook.json');
 
 export async function getWebHook() {
-  const files = await readdir(path.join('/tmp', 'hook.json'));
+  const files = await readdir('/tmp');
   for (const file1 of files) console.log(file1, 'filess');
 
   const rawFileContent = fs.readFileSync(file, { encoding: 'utf-8' });
   const data = JSON.parse(rawFileContent);
+
+  console.log(data, 'ddd');
 
   const hookURLS = data.webhooks ?? [];
   return hookURLS;
