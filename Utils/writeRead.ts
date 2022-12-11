a8ce7f1e4e17a1f06d94b5f899c0e7f802541b6f
rhutmann <pappy.rhutmannjnrolege@gmail.com>
2022-12-11 10:07:59 +0000

    added services

--- Utils/writeRead.ts
+++ Utils/writeRead.ts
@@ -4,19 +4,18 @@ import path from 'path';
 const file =
   process.env.NODE_ENV !== 'production'
     ? path.join(process.cwd(), '/webhooks/data.json')
-    : path.join(process.cwd(), '/../../../../../temp', 'hook.json');
+    : path.join('/temp', 'hook.json');
 
 export async function getWebHook() {
-  const files = await fs.readdir(
-    path.join(process.cwd(), '/../../../../../temp', 'hook.json')
-  );
+  const files = await fs.readdir(path.join('/temp', 'hook.json'));
   for (const file1 of files) console.log(file1, 'filess');
 
-  const rawFileContent = await fs.readFile(file, { encoding: 'utf-8' });
-  const data = JSON.parse(rawFileContent);
+  // const rawFileContent = await fs.readFile(file, { encoding: 'utf-8' });
+  // const data = JSON.parse(rawFileContent);
 
-  const hookURLS = data.webhooks ?? [];
-  return hookURLS;
+  // const hookURLS = data.webhooks ?? [];
+  // return hookURLS;
+  return [];
 }
 
 export function storeHooks(webhooks: any) {
